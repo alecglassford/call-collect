75941fa038e431b4d8d748073f2b128537fb5cc6
Alec Glassford <glassford@stanford.edu>
2018-03-03 17:31:50 -0800

    Alphabetize project list

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,7 +2,9 @@ import db from '../db';
 
 export default async function getProjects(req, res) {
   try {
-    const projects = await db('projects').select().all();
+    const projects = await db('projects').select({
+      sort: [{ field: 'name', direction: 'asc' }],
+    }).all();
     res.json(projects);
   } catch (err) {
     res.sendStatus(500);

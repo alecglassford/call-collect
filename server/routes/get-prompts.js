b608563db740144bfd05d266c06217aea6300a3f
Alec Glassford <glassford@stanford.edu>
2018-03-04 17:17:11 -0800

    Upload prompt on project page (cloes #14, toward #15, toward #16)

--- server/routes/get-prompts.js
+++ server/routes/get-prompts.js
@@ -1,13 +1,23 @@
 import db from '../db';
 
 export default async function getPrompts(req, res) {
+  const hostname = process.env.NOW_URL || `${req.protocol}://${req.hostname}`;
   const { projectName } = req.params;
   try {
-    const prompts = await db('prompts').select({
-      filterByFormula: `project='${projectName}'`,
-      sort: [{ field: 'index', direction: 'asc' }],
-    }).all();
-    res.json(prompts);
+    while (true) { // eslint-disable-next-line no-await-in-loop
+      const prompts = await db('prompts').select({
+        filterByFormula: `project='${projectName}'`,
+        sort: [{ field: 'index', direction: 'asc' }],
+      }).all();
+      // Make sure we don't have any temp-serve URLs for audio.
+      if (prompts.every(p => !p.fields.audio[0] || !p.fields.audio[0].url.startsWith(hostname))) {
+        res.json(prompts);
+        return;
+      }
+      // If we do, wait a second and try again (hopefully Airable will have processed)
+      // eslint-disable-next-line no-await-in-loop
+      await new Promise((resolve) => { setTimeout(resolve, 1000); });
+    }
   } catch (err) {
     console.error(err);
     res.sendStatus(500);
--- server/routes/new-prompt.js
+++ server/routes/new-prompt.js
@@ -4,16 +4,20 @@ import db from '../db';
 // and we have req.body.projectId, req.body.index
 export default async function newPrompt(req, res) {
   const hostname = process.env.NOW_URL || `${req.protocol}://${req.hostname}`;
-  const { index, projectId } = req.body;
+  const { index, slug, projectId } = req.body;
   const filePath = encodeURIComponent(req.file.path);
   const fileMime = encodeURIComponent(req.file.mimetype);
   try {
-    const newPromptRecord = await db('prompts').create({
+    await db('prompts').create({
+      slug,
       index: +index,
       project: [projectId],
       audio: [{ url: `${hostname}/api/temp?filepath=${filePath}&mimetype=${fileMime}` }],
     });
-    res.send(newPromptRecord);
+    res.sendStatus(200);
+    // No point in sending actual prompt obj, because audio won't be ready in Airtable
+    // Instead we'll just refetch from get-prompts if needed: one source of prompt data
+    // May be slower, but easier to deal with, and we'll check there to make sure audio is ready
   } catch (err) {
     res.sendStatus(500);
   }

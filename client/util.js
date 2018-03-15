97f744c32d1550a29c53fbeed3a785ba060d2842
Alec Glassford <glassford@stanford.edu>
2018-03-15 02:39:22 -0700

    Revamp submissions (closes #15)

--- client/util.js
+++ client/util.js
@@ -5,6 +5,12 @@ export const humanPhone = function humanPhoneFunc(phone) {
 };
 
 // TK this is a dumb function, man. Switch out for just adding 1 everywhere.
-export const humanIndex = function humanIndex(index) {
+export const humanIndex = function humanIndexFunc(index) {
   return index + 1;
+};
+
+export const subToPromptName = function subToPromptNameFunc(sub, currentPrompts) {
+  const promptId = sub.fields.prompt[0];
+  const prompt = currentPrompts.find(p => p.id === promptId);
+  return prompt.fields.slug || `Prompt ${prompt.fields.index + 1}`;
 };

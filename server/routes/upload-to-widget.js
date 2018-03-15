294c95cddd7b9af0dd31e68da14c99cdfaf37467
Alec Glassford <glassford@stanford.edu>
2018-03-15 11:01:48 -0700

    Speed up widget upload for caller, hide download button (in chrome at least)

--- server/routes/upload-to-widget.js
+++ server/routes/upload-to-widget.js
@@ -1,25 +1,26 @@
 import db from '../db';
 import transcribe from '../transcribe-submission';
 
-export default async function uploadToWidget(req, res) {
+const saveAndTranscribe = async function saveAndTranscribeFunc(req) {
   const hostname = process.env.NOW_URL || `${req.protocol}://${req.hostname}`;
   const { contact, projectId, promptId } = req.body;
   const filePath = encodeURIComponent(req.file.path);
   const fileMime = encodeURIComponent(req.file.mimetype);
-  console.log(hostname, contact, projectId, promptId, filePath, fileMime);
-  try {
-    const submission = await db('submissions').create({
-      caller: `${contact} (${req.ip})`,
-      project: [projectId],
-      prompt: [promptId],
-      widget_audio: [{ url: `${hostname}/api/temp?filepath=${filePath}&mimetype=${fileMime}` }],
-      audio: req.file.filename, // This is just a meaningless unique ID really
-    });
-    transcribe(submission);
-    res.sendStatus(200);
-  } catch (err) {
-    // TK handle err better
-    console.error(err);
-    res.sendStatus(500);
-  }
+  const submission = await db('submissions').create({
+    caller: `${contact} (${req.ip})`,
+    project: [projectId],
+    prompt: [promptId],
+    widget_audio: [{ url: `${hostname}/api/temp?filepath=${filePath}&mimetype=${fileMime}` }],
+    audio: req.file.filename, // This is just a meaningless unique ID really
+  });
+  transcribe(submission);
+};
+
+// Respond to request *immediately*
+// Caller won't know if there's an error, buut it's a lot quicker this way!
+export default async function uploadToWidget(req, res) {
+  saveAndTranscribe(req).catch((err) => {
+    console.error('Error saving and transcribing from widget:', err);
+  });
+  res.sendStatus(200);
 }

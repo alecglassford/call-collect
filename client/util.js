64c6bed85b373b9760bd390b903987e435161308
Alec Glassford <glassford@stanford.edu>
2018-03-14 18:40:17 -0700

    Can view/hear  widget submissions/transcriptions in dashboard. Sucky UI, but yay

--- client/util.js
+++ client/util.js
@@ -1,5 +1,6 @@
 export const humanPhone = function humanPhoneFunc(phone) {
   if (!phone) return 'No phone number';
+  if (!phone.startsWith('+') || !phone.length === 12) return phone;
   return `${phone.substring(2, 5)}-${phone.substring(5, 8)}-${phone.substring(8, 12)}`;
 };
 
